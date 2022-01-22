730d1d0d3e6ed9fdb5636b20fbf09cbd7c39bdad
SanghoonLee <youlovenot@naver.com>
2022-01-22 17:52:11 +0900

    [2022.01.22] second JDBC commit

--- pop games main/src/main/webapp/SQL_script.sql
+++ pop games main/src/main/webapp/SQL_script.sql
@@ -6,10 +6,12 @@ create table member(
 	id varchar(20) primary key,
 	password varchar(20) not null,
 	name varchar(20) not null,
-	phone varchar(20) not null,
+	phone varchar(20),
 	email varchar(20),
-	nickname varchar(20),
 	zipcode varchar(20),
 	adress1 varchar(20),
 	adress2 varchar(20)
     );
+
+insert into member(id,password,name)
+values("admin","admin1234","관리자");
