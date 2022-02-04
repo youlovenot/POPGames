fbf84556d2b807cc1c64993a30114a7d84ce9ade
SanghoonLee <youlovenot@naver.com>
2022-02-04 18:06:17 +0900

    [2022.02.04] mybatis 연동중인데 왜안될까 commit

---
+++ pop games main/src/main/java/com/springbook/biz/notice/impl/NoticeDAOMybatis.java
@@ -0,0 +1,41 @@
+package com.springbook.biz.notice.impl;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.springbook.biz.notice.NoticeVO;
+
+@Repository
+public class NoticeDAOMybatis extends SqlSessionDaoSupport {
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	public void insertNotice(NoticeVO vo) {
+		System.out.println("===> Mybatis로 insertNotice() 기능 처리");
+		mybatis.insert("NoticeDAO.insertNotice", vo);
+	}
+	
+	public void updateNotice(NoticeVO vo) {
+		System.out.println("===> Mybatis로 updateNotice() 기능 처리");
+		mybatis.update("NoticeDAO.updateNotice", vo);
+	}
+	
+	public void deleteNotice(NoticeVO vo) {
+		System.out.println("===> Mybatis로 deleteNotice() 기능 처리");
+		mybatis.delete("NoticeDAO.deleteNotice", vo);
+	}
+	
+	public NoticeVO getNotice(NoticeVO vo) {
+		System.out.println("===> Mybatis로 getNotice() 기능 처리");
+		return mybatis.selectOne("NoticeDAO.getNotice", vo);
+	}
+	
+	public List<NoticeVO> getNoticeList(NoticeVO vo) {
+		System.out.println("===> Mybatis로 getNoticeList() 기능 처리");
+		return mybatis.selectList("NoticeDAO.getNoticeList", vo);
+	}
+}
--- pop games main/src/main/webapp/SQL_script.sql
+++ pop games main/src/main/webapp/SQL_script.sql
@@ -18,7 +18,7 @@ values('admin','admin1234', '관리자');
 insert into member(id, password, name)
 values('test','test123','테스트');
 insert into member
-values('youlovenot','Pa$$w0rd', '이상훈','010-1111-1111', 'youlovenot@naver.com','123-456','메롱시티','파인에플집'); 
+values('userID','Pa$$w0rd', '사용자','010-1111-1111', 'email@naver.com','123-456','메롱시티','파인에플집'); 
 
 select * from member;
 
